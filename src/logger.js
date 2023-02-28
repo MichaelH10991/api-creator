5ca6f2bbf3febdf05740301845753e9526daec8a
Michael Humphries <michaelhumphries1997@gmail.com>
2023-02-28 09:44:22 +0000

    removing log to file

--- src/logger.js
+++ src/logger.js
@@ -11,8 +11,8 @@ const init = (config) => {
       // - Write all logs with importance level of `error` or less to `error.log`
       // - Write all logs with importance level of `info` or less to `combined.log`
       //
-      new winston.transports.File({ filename: 'error.log', level: 'error' }),
-      new winston.transports.File({ filename: 'combined.log' }),
+      // new winston.transports.File({ filename: 'error.log', level: 'error' }),
+      // new winston.transports.File({ filename: 'combined.log' }),
     ],
   });
 
